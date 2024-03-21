f3fc2bc38d8c891ec0d27534774f14ea603c331d
Alex H <53427235+Ahureau@users.noreply.github.com>
2024-03-21 14:57:54 +0100

    Can now play multiple rounds

--- scripts.js
+++ scripts.js
@@ -8,31 +8,59 @@ function getComputerChoice() {
     }
 }
 
-let computerSelection = getComputerChoice();
-let playerSelection = prompt("Rock, paper, or scissors?");
+let computerScore = 0;
+let playerScore = 0;
 
 function playRound() {
+    let computerSelection = getComputerChoice();
+    let playerSelection = prompt("Rock, paper, or scissors?");
     playerSelection = playerSelection.toLowerCase();
     if (computerSelection === "rock" && playerSelection === "scissors") {
+        computerScore++;
         return "You lose! Rock beats scissors.";
     } else if (computerSelection === "rock" && playerSelection === "paper") {
+        playerScore++;
         return "You win! Paper beats rock.";
     } else if (computerSelection === "paper" && playerSelection === "rock") {
+        computerScore++;
         return "You lose! Rock beats scissors.";
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        playerScore++;
         return "You win! Scissors beat paper.";
     } else if (computerSelection === "scissors" && playerSelection === "scissors") {
+        computerScore++;
         return "You lose! scissors beats paper.";
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        computerScore++;
         return "You lose! Scissors beat paper.";
     } else if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-        return "That's not a valid choice. Pick rock, paper, or scissors.";
+        computerScore++;
+        return "That's not a valid choice. Pick rock, paper, or scissors next round.";
     } else {
         return "It's a tie!";
     }
 }
 
-console.log(playRound());
+function winner() {
+    if (computerScore > playerScore) {
+        console.log("You lose!");
+    } else if (computerScore < playerScore) {
+        console.log("You win!");
+    } else {
+        console.log("It's a tie!");
+    }
+}
+
+function playGame(){
+    let round;
+    for (round = 1; round =< 5; round++) {
+        console.log("Round " + round)
+        console.log(playRound());
+    }
+    winner();
+}
+
+playGame();
 
 /* Just trying something more complicated than needed.
 
